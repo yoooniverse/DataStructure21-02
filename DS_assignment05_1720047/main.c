198c1a08caf620afe09eb758cfc4fe4dfc240eb3
TessYKLEE <hiway60510@gmail.com>
2021-10-27 16:23:06 +0900

    Initial Commit

---
+++ DS_assignment05_1720047/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DS_assignment05_1720047
+//
+//  Created by YOONKYUNG LEE on 2021/10/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
