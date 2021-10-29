2ac27c974d394c123e2e30ffcefeab133657d52b
TessYKLEE <hiway60510@gmail.com>
2021-10-29 11:37:10 +0900

    5-2까지 끝냄

--- DS_assignment05-02_1720047/main.c
+++ DS_assignment05-02_1720047/main.c
@@ -6,10 +6,63 @@
 //  
 //
 
+//Implement the function that finds a predecessor of node in the inorder traversal.
+
+//            G7
+//       C3       F6
+//    A1   B2   D4   E5
+
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+typedef struct TreeNode {
+    int data;
+    struct TreeNode *left, *right, *parent;
+} TreeNode;
+
+
+TreeNode n1 = { 'A', NULL, NULL, NULL };
+TreeNode n2 = { 'B', NULL, NULL, NULL };
+TreeNode n3 = { 'C', &n1, &n2, NULL };
+TreeNode n4 = { 'D', NULL, NULL, NULL };
+TreeNode n5 = { 'E', NULL, NULL, NULL };
+TreeNode n6 = { 'F', &n4, &n5, NULL };
+TreeNode n7 = { 'G', &n3, &n6, NULL };
+TreeNode *exp_root = &n7;
+
+//주어진 노드의 이전 노드 찾기.
+TreeNode *tree_predecessor(TreeNode *p) {
+    if (p->left != NULL) {
+        p = p->left;   //p를 오른쪽 서브트리로 지정한다음
+        while (p->right) p = p->right;    //가장 왼쪽에 있는 노드까지 가도록 while 반복 사용
+        return p;   //가장 왼쪽으로 가면 해당 노드를 반납한다. 그게 이전 노드의 successor가 됨.
+    }
+    
+    TreeNode *y = p->parent;
+    while (y != NULL && p == y->left) {
+        p = y;
+        y = y->parent;
+    }
+    return y;
+}
+
+
+int main(void) {
+    TreeNode *q = exp_root;
+    n1.parent = &n3;
+    n2.parent = &n3;
+    n3.parent = &n7;
+    n4.parent = &n6;
+    n5.parent = &n6;
+    n6.parent = &n7;
+    n7.parent = NULL;
+    
+    //go to the rightmost node
+    while (q->right) q = q->right;
+    
+    do {
+        printf("%c\n", q->data);
+        q = tree_predecessor(q);
+    } while(q);
+    
     return 0;
 }
